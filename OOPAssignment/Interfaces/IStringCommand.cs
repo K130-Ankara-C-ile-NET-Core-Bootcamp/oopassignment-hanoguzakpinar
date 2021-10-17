71b8ed0bafd18117b30fa3367f2f2bf63483dfca
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-10-17 20:02:49 +0300

    last commit.

--- OOPAssignment/Interfaces/IStringCommand.cs
+++ OOPAssignment/Interfaces/IStringCommand.cs
@@ -1,11 +1,6 @@
-using System;
-
 namespace OOPAssignment.Interfaces
 {
     public interface IStringCommand : ICommand<string>
     {
-        ///
-        public new void ExecuteCommand(string commandObject);
-        ///
     }
 }
